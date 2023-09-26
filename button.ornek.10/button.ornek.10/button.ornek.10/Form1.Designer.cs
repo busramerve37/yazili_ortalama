996d3793b61b462b7d19520809f2dfed3328d840
busramerve37 <145451633+busramerve37@users.noreply.github.com>
2023-09-26 16:37:05 +0300

    Add files via upload

---
+++ button.ornek.10/button.ornek.10/button.ornek.10/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace button.ornek._10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtYazili1 = new System.Windows.Forms.TextBox();
+            this.txtYazili2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOrtalama = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(31, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Yazılı1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(31, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Yazılı 2";
+            // 
+            // txtYazili1
+            // 
+            this.txtYazili1.Location = new System.Drawing.Point(137, 35);
+            this.txtYazili1.Name = "txtYazili1";
+            this.txtYazili1.Size = new System.Drawing.Size(100, 20);
+            this.txtYazili1.TabIndex = 2;
+            // 
+            // txtYazili2
+            // 
+            this.txtYazili2.Location = new System.Drawing.Point(137, 74);
+            this.txtYazili2.Name = "txtYazili2";
+            this.txtYazili2.Size = new System.Drawing.Size(100, 20);
+            this.txtYazili2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(31, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ortalama";
+            // 
+            // txtOrtalama
+            // 
+            this.txtOrtalama.Location = new System.Drawing.Point(155, 212);
+            this.txtOrtalama.Name = "txtOrtalama";
+            this.txtOrtalama.Size = new System.Drawing.Size(100, 20);
+            this.txtOrtalama.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 61);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 398);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtOrtalama);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtYazili2);
+            this.Controls.Add(this.txtYazili1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtYazili1;
+        private System.Windows.Forms.TextBox txtYazili2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOrtalama;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
